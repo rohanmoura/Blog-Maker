e3b71b370bef5204a94577d0ae81b3de46de9344
rohanmoura <rsmoura29@gmail.com>
2024-08-24 18:07:01 +0530

    Articles Overview

--- app/dashboard/sites/[siteId]/page.tsx
+++ app/dashboard/sites/[siteId]/page.tsx
@@ -1,10 +1,50 @@
+import EmptyState from '@/app/components/dashboard/EmptyState';
+import { Button } from '@/components/ui/button';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { Book, PlusCircle, Settings } from 'lucide-react';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const SiteId = () => {
+const SiteId: React.FC<{ params: { siteId: string } }> = async ({ params }) => {
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+
+
   return (
-    <div>
-      Site Id Page
-    </div>
+    <>
+      <div className="flex w-full justify-end gap-x-4">
+        <Button asChild variant={"secondary"}>
+          <Link href={"#"}>
+            <Book className='size-4 mr-2' />
+            View Blog
+          </Link>
+        </Button>
+        <Button asChild variant="secondary">
+          <Link href={"#"}>
+            <Settings className="size-4 mr-2" />
+            Settings
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href={"#"}>
+            <PlusCircle className="size-4 mr-2" />
+            Create Article
+          </Link>
+        </Button>
+      </div>
+      <EmptyState
+        title="You dont have any Articles created"
+        description="You currently dont have any articles. please create some so that you can see them right here"
+        buttonText="Create Article"
+        href={`/dashboard/sites/${params.siteId}/create`}
+      />
+    </>
   )
 }
 
